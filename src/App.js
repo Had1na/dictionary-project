0d80c021deaec5c9978895b97b7812788e935a55
Had1na <gadubas@gmail.com>
2024-10-17 11:52:30 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./mcw-logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="App-logo img-fluid" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
