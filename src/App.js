14ebcf71fdd5d5215bbaefd64085de69f5ca1de7
Had1na <gadubas@gmail.com>
2024-10-17 12:21:13 +0100

    Added basic search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import logo from "./mcw-logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" className="App-logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Gabriella Dubas</footer>
+      </div>
     </div>
   );
 }
