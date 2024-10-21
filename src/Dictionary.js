86e6d86b9bfa1be0b0c90165a62b2593060bc78f
Had1na <gadubas@gmail.com>
2024-10-21 17:09:39 +0100

    Added Photos with API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
+import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
+  function handleImageResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
-    let apiKey = "137ae6afd69a1ab4tc7ba7202of80b98";
+    const apiKey = "137ae6afd69a1ab4tc7ba7202of80b98";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imageApiUrl).then(handleImageResponse);
   }
 
   function handleSubmit(event) {
@@ -49,6 +58,7 @@ export default function Dictionary(props) {
           <div className="hint">Suggested words: sunset, sake, bonsai... </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
